fb7680281db0b25b66844e371d8de857d0aae736
Minmin Gong <mgong@microsoft.com>
2018-11-14 00:06:08 -0800

    [CI][Linux][macOS] Linux and macOS support
    1. Replace WRL::ComPtr with CComPtr,
    2. Cross platform UUID,
    3. Add compiler options for GCC and Clang,
    4. Add travis rules,

--- BuildAll.py
+++ BuildAll.py
@@ -51,13 +51,22 @@ if __name__ == "__main__":
 	if not os.path.exists("Build"):
 		os.mkdir("Build")
 
-	vs2017Folder = FindVS2017Folder(FindProgramFilesFolder())
+	host_platform = sys.platform
+	if 0 == host_platform.find("win"):
+		host_platform = "win"
+	elif 0 == host_platform.find("linux"):
+		host_platform = "linux"
+	elif 0 == host_platform.find("darwin"):
+		host_platform = "darwin"
 
 	argc = len(sys.argv);
 	if (argc > 1):
 		buildSys = sys.argv[1]
 	else:
-		buildSys = "vs2017"
+		if host_platform == "win":
+			buildSys = "vs2017"
+		else:
+			buildSys = "ninja"
 	if (argc > 2):
 		arch = sys.argv[2]
 	else:
@@ -69,13 +78,6 @@ if __name__ == "__main__":
 
 	multiConfig = (buildSys.find("vs") == 0)
 
-	if "x64" == arch:
-		vcOption = "amd64"
-	elif "x86" == arch:
-		vcOption = "x86"
-	else:
-		LogError("Unsupported architecture.\n")
-
 	buildDir = "Build/%s-%s" % (buildSys, arch)
 	if not multiConfig:
 		buildDir += "-%s" % configuration;
@@ -86,11 +88,32 @@ if __name__ == "__main__":
 
 	parallel = multiprocessing.cpu_count()
 
-	cmd = "%sVCVARSALL.BAT %s && cd /d \"%s\"" % (vs2017Folder, vcOption, buildDir)
+	cmdList = []
+	if host_platform == "win":
+		vs2017Folder = FindVS2017Folder(FindProgramFilesFolder())
+		if "x64" == arch:
+			vcOption = "amd64"
+		elif "x86" == arch:
+			vcOption = "x86"
+		else:
+			LogError("Unsupported architecture.\n")
+		cmdList.append("%sVCVARSALL.BAT %s && cd /d \"%s\"" % (vs2017Folder, vcOption, buildDir))
 	if (buildSys == "ninja"):
-		cmd += " && set CC=cl.exe && set CXX=cl.exe && cmake -G Ninja -DCMAKE_BUILD_TYPE:STRING=\"%s\" ../../ && ninja -j%d" % (configuration, parallel)
+		if host_platform == "win":
+			cmdList.append("set CC=cl.exe")
+			cmdList.append("set CXX=cl.exe")
+		cmdList.append("cmake -G Ninja -DCMAKE_BUILD_TYPE=\"%s\" -DSC_ARCH_NAME=\"%s\" ../../" % (configuration, arch))
+		cmdList.append("ninja -j%d" % parallel)
 	else:
-		cmd += " && cmake -G \"Visual Studio 15\" -T host=x64 -A %s ../../ && MSBuild ALL_BUILD.vcxproj /nologo /m:%d /v:m /p:Configuration=%s,Platform=%s" % (arch, parallel, configuration, arch)
+		cmdList.append("cmake -G \"Visual Studio 15\" -T host=x64 -A %s ../../" % arch)
+		cmdList.append("MSBuild ALL_BUILD.vcxproj /nologo /m:%d /v:m /p:Configuration=%s,Platform=%s" % (parallel, configuration, arch))
+	cmd = ""
+	for i in range(0, len(cmdList)):
+		cmd += cmdList[i]
+		if i != len(cmdList) - 1:
+			cmd += " && "
+	print(buildDir)
+	print(cmd)
 	subprocess.call(cmd)
 
 	os.chdir(originalDir)
